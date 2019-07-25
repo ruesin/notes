4751dae70f031999d3dcc9033ced6cbb59eb3266
ruesin <ruesin@gmail.com>
2019-07-25 23:06:08 +0800

    update shell

--- scripts/hexo.sh
+++ scripts/hexo.sh
@@ -3,15 +3,15 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 cd $SHELL_FOLDER
 
-if [ ! -d "./.hexo" ]; then
-  git clone git@github.com:ruesin/hexo.git .hexo
-  cd .hexo
+if [ ! -d "../.hexo" ]; then
+  git clone git@github.com:ruesin/hexo.git ../.hexo
+  cd ../.hexo
   npm install
   cd $SHELL_FOLDER
 fi
 
 # clean
-cd .hexo
+cd ../.hexo
 hexo clean
 
 rm -rf ./source/_posts ./source/images
@@ -21,6 +21,7 @@ fi
 if [ ! -d "./source/images/" ]; then
   mkdir -p ./source/images/
 fi
+
 cp -r ../docs/ ./source/_posts/
 cp -r ../images/ ./source/images/
 
